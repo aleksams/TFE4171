4cb34111c5c33e9dc24c3bbc9eca918f39ad5e1f
AleksanderSkarnes <aleksams@stud.ntnu.no>
2019-04-15 12:01:34 +0200

    Fixes dropped verification

--- testPr_hdlc.sv
+++ testPr_hdlc.sv
@@ -212,7 +212,7 @@ program testPr_hdlc(
 
     if(Abort)
       InsertFlagOrAbort(0);
-    else if(Dropped)
+    else if(Drop)
       WriteAddress(`Rx_SC, 8'h22);
     else
       InsertFlagOrAbort(1);
@@ -336,7 +336,7 @@ program testPr_hdlc(
         end
 
     // Verify content of Rx_Buff registers
-    for(int i=0; i<`RX_BUFF_SIZE; i++) begin
+    for(int i=0; i<Size; i++) begin
       ReadAddress(`Rx_Buff, ReadData);
       a_overflow_RxBuff_content: assert (ReadData == data[i]) else begin
         $display("ERROR: RX_BUFF[%0d]=%8b, not the correct value after overflow receive, should be: %8b!", i, ReadData, data[i]);
@@ -362,7 +362,7 @@ program testPr_hdlc(
       end
 
   // Verify content of Rx_Buff registers
-  for(int i=0; i<Size+2; i++) begin
+  for(int i=0; i<Size; i++) begin
     ReadAddress(`Rx_Buff, ReadData);
     a_abort_RxBuff_content: assert (ReadData == 0) else begin
       $display("ERROR: RX_BUFF[%0d]=%0b, not the correct value after FCS error receive!", i, ReadData);
@@ -390,16 +390,16 @@ program testPr_hdlc(
     Receive( Size, 0, 0, 0, 0, 1, 0, data); //Dropped Frame
 
     ReadAddress(`Rx_SC, ReadData);
-    a_dropped_RXSC_content: assert (ReadData == 8'b00100001) $display ("PASS: VerifyDroppedFrameRXSC, RX_SC=%8b", ReadData);
+    a_dropped_RXSC_content: assert (ReadData == 8'b00101000) $display ("PASS: VerifyDroppedFrameRXSC, RX_SC=%8b", ReadData);
         else begin
           $display("ERROR: RX_SC=%8b, not the correct value after Dropped frame receive!", ReadData);
           TbErrorCnt++;
         end
 
     // Verify content of Rx_Buff registers
-    for(int i=0; i<Size+2; i++) begin
+    for(int i=0; i<Size; i++) begin
       ReadAddress(`Rx_Buff, ReadData);
-      a_dropped_RxBuff_content: assert (ReadData == 0) else begin
+      a_dropped_RxBuff_content: assert (ReadData == data[i]) else begin
         $display("ERROR: RX_BUFF[%0d]=%0b, not the correct value after Dropped frame receive!", i, ReadData);
         TbErrorCnt++;
       end
