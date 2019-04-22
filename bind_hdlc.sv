85ba4f06a36006fce9e1217567f428f8079b467c
Halvor Horvei <halvho@stud.ntnu.no>
2019-04-22 12:05:08 +0200

    AbortSignal bind error fix

--- bind_hdlc.sv
+++ bind_hdlc.sv
@@ -10,13 +10,11 @@ module bind_hdlc ();
     .ErrCntAssertions(uin_hdlc.ErrCntAssertions),
     .Clk(uin_hdlc.Clk),
     .Rst(uin_hdlc.Rst),
-    
     .Rx(uin_hdlc.Rx),
     .Rx_FlagDetect(uin_hdlc.Rx_FlagDetect),
     .Rx_ValidFrame(uin_hdlc.Rx_ValidFrame),
     .Rx_AbortSignal(uin_hdlc.Rx_ValidFrame),
     .Rx_AbortDetect(uin_hdlc.Rx_AbortDetect),
-
     .Tx(uin_hdlc.Tx),
     .Tx_ValidFrame(uin_hdlc.Tx_ValidFrame)
   );
