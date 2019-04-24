bf9d627c692a05a32881539e71f7d72f67aa8d96
Halvor Horvei <halvho@stud.ntnu.no>
2019-04-24 16:09:25 +0200

    disable iff rst

--- assertions_hdlc.sv
+++ assertions_hdlc.sv
@@ -74,7 +74,7 @@ module assertions_hdlc (
 
 
   property Generate_EndofFrame_Flag;
-    @(posedge Clk) $fell(Tx_ValidFrame) and !Tx_AbortedTrans |-> ##1 Tx_Flag;
+    @(posedge Clk) disable iff(rst) $fell(Tx_ValidFrame) and !Tx_AbortedTrans |-> ##1 Tx_Flag;
 endproperty
 
 Generate_EndofFrame_Flag_Assert    :  assert property (Generate_EndofFrame_Flag) $display("PASS: Generate_EndofFrame_Flag_flag");
