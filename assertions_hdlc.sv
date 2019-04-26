da7da51d854e9a66cdfecfdfd6d687f386940927
Halvor Horvei <halvho@stud.ntnu.no>
2019-04-26 16:41:30 +0200

    typecast

--- assertions_hdlc.sv
+++ assertions_hdlc.sv
@@ -147,7 +147,7 @@ module assertions_hdlc (
                                     else begin $display("ERROR(%0t): Rx_EoF did not go high after Rx_FlagDetect during validframe",$time); ErrCntAssertions++; end
 
   property RX_ReadReady;
-    @(posedge Clk) Rx_EoF and !Rx_AbortSignal and !Rx_FrameError |-> ##0 Rx_Ready [*(Rx_FrameSize+2)]; // or Rx_Ready throughout  Rx_Drop [->1];
+    @(posedge Clk) Rx_EoF and !Rx_AbortSignal and !Rx_FrameError |-> ##0 Rx_Ready [*(int(Rx_FrameSize)+2)]; // or Rx_Ready throughout  Rx_Drop [->1];
   endproperty
 
   RX_ReadReady_Assert            : assert property (RX_ReadReady)$display("PASS: RX_ReadReady");
